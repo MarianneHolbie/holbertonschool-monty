b8ad9030f0acb9b1606dea2e4cba5ddf24178fa7
paulinepar <paulineparmigiani@gmail.com>
2022-12-14 16:02:20 +0100

    correction commentaire for compile checker

--- op_func1.c
+++ op_func1.c
@@ -55,24 +55,15 @@ void push(stack_t **stack, unsigned int line_number)
  * @line_number: unsigned int
  * Description: function that print with opcodes pall
  * retur: 0
-
-
-
-
- *void pint(stack_t **stack, unsigned int line_number)
- *{
+ * void pint(stack_t **stack, unsigned int line_number)
+ * {
  *	(void)(line_number);
-
-
  * }
-
  * void pop - function to del element in a stack
  * @stack: double pointer
  * @line_number: unsigned int
  * Description: function that print with opcodes pall
  * retur: 0
-
-
  * void pop(stack_t **stack, unsigned int line_number)
  * {
  *	(void)(line_number);
